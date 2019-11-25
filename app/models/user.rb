4d700014e1dcd18dbafc09851cebffdda3feae4a
Hector Acosta <hector.acosta.pozo@outlook.com>
2019-11-25 15:10:40 -0400

    Added env for db

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :username, presence: true, uniqueness: true
-  validates :username, format: { width: VALID_USERNAME_REGEX }
+  validates :username, presence: true, uniqueness: true, format: { width: VALID_USERNAME_REGEX }
   validates :password,
             length: { minimum: 6 },
             if: -> { new_record? || !password.nil? }
